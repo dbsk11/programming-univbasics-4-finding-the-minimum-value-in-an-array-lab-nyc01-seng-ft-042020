930497036b32d0affdb15d0dabc3e2830cb8e894
Jessica K <jessikim807@gmail.com>
2020-04-08 16:11:32 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -2,9 +2,7 @@ def find_min_value(array)
   counter = 0
   min_value = -1
   while counter < array.length do
-    if min_value < array[counter]
-      min_value = array[counter]
-    end
+    array.min 
     counter += 1
   end
   min_value
